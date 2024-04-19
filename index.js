d5ab40a56fa3b94d4e2686b9533bd06e6bb7dc36
Djek <ruffino.jeremie@gmail.com>
2024-04-19 10:29:48 +0300

    Update helius API Key

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f9c7f1de83dffb909e9ade43b35cb2f5ccb5443c1a6ca955271a2f43c6c5928e
-size 379
+const { Connection, PublicKey } = require ("@solana/web3.js");
+
+const solanaInfo = async (address) => {
+  const connection = new Connection("https://mainnet.helius-rpc.com/?api-key=34e1fd3a-1147-4e19-9d8f-0896d0eee4ce");
+  let info = await connection.getAccountInfo(new PublicKey(address))
+  return info
+}
+
+const sum = (a, b) => a + b
+
+exports.sum = sum
+exports.info = solanaInfo
